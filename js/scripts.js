2e8956a4ff8c7225ebc94a0574fc7a687ada381c
Narayanan Ammanur <nammanur@gmail.com>
2022-02-11 12:12:08 +0530

    commits for Bond Trading

--- js/scripts.js
+++ js/scripts.js
@@ -1,59 +1,56 @@
-/*!
-* Start Bootstrap - Creative v7.0.5 (https://startbootstrap.com/theme/creative)
-* Copyright 2013-2021 Start Bootstrap
-* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-creative/blob/master/LICENSE)
-*/
-//
-// Scripts
-// 
-
-window.addEventListener('DOMContentLoaded', event => {
-
-    // Navbar shrink function
-    var navbarShrink = function () {
-        const navbarCollapsible = document.body.querySelector('#mainNav');
-        if (!navbarCollapsible) {
-            return;
-        }
-        if (window.scrollY === 0) {
-            navbarCollapsible.classList.remove('navbar-shrink')
+$(function(){
+    $(".carousel").carousel({interval:2000});
+    $("#carouselButton").click(function() {
+        if ( $("#carouselButton").children("i").hasClass("fa-pause")) {
+            $(".carousel").carousel("pause");
+            $("#carouselButton").children("i").removeClass("fa-pause");
+            $("#carouselButton").children("i").addClass("fa-play");
         } else {
-            navbarCollapsible.classList.add('navbar-shrink')
+            $(".carousel").carousel("cycle");
+            $("#carouselButton").children("i").removeClass("fa-play");
+            $("#carouselButton").children("i").addClass("fa-pause");
+        }
+    });
+})
+
+
+
+
+$(function() {
+    'use strict';
+    function remoteModal(idModal){
+        var vm = this;
+        vm.modal = $(idModal);
+
+        if( vm.modal.length == 0 ) {
+            return false;
         }
 
-    };
+        if( window.location.hash == idModal ){
+            openModal();
+        }
 
-    // Shrink the navbar 
-    navbarShrink();
+        var services = {
+            open: openModal,
+            close: closeModal
+        };
 
-    // Shrink the navbar when page is scrolled
-    document.addEventListener('scroll', navbarShrink);
+        return services;
+        ///////////////
 
-    // Activate Bootstrap scrollspy on the main nav element
-    const mainNav = document.body.querySelector('#mainNav');
-    if (mainNav) {
-        new bootstrap.ScrollSpy(document.body, {
-            target: '#mainNav',
-            offset: 74,
-        });
-    };
+        // method to open modal
+        function openModal(){
+            vm.modal.modal('show');
+        }
 
-    // Collapse responsive navbar when toggler is visible
-    const navbarToggler = document.body.querySelector('.navbar-toggler');
-    const responsiveNavItems = [].slice.call(
-        document.querySelectorAll('#navbarResponsive .nav-link')
-    );
-    responsiveNavItems.map(function (responsiveNavItem) {
-        responsiveNavItem.addEventListener('click', () => {
-            if (window.getComputedStyle(navbarToggler).display !== 'none') {
-                navbarToggler.click();
-            }
-        });
-    });
+        // method to close modal
+        function closeModal(){
+            vm.modal.modal('hide');
+        }
+    }
+    Window.prototype.remoteModal = remoteModal;
+});
 
-    // Activate SimpleLightbox plugin for portfolio items
-    new SimpleLightbox({
-        elements: '#portfolio a.portfolio-box'
-    });
-
+$(function(){
+    window.remoteModal('#loginModal');
 });
